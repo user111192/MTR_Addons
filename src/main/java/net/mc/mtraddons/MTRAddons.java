8e61d1c0850c6a61cad77fe9d5130bde5f1c353b
chenziang <3538995003@qq.com>
2022-11-13 13:56:36 +0800

    update modid

--- src/main/java/net/mc/mtraddons/MTRAddons.java
+++ src/main/java/net/mc/mtraddons/MTRAddons.java
@@ -30,7 +30,7 @@ import org.slf4j.Logger;
 public class MTRAddons {
 
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "MTRAddons";
+    public static final String MODID = "mtr_addons";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "MTRAddons" namespace
